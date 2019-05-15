081565c9ae2a30e596fbfbd3951ee22e1b8a0f16
Jonathan Luk <jluk87@gmail.com>
2019-05-15 03:15:58 +0000

    Automatically backed up by Learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,7 +7,7 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by(name: params[:user][:name])
     binding.pry
-    if user.try(:authenicate, params[:user][:password])
+    if user.try(:authenticate, params[:user][:password])
       session[:user_id] = user.id
       binding.pry
       redirect_to welcome_index_path
