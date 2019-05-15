a97e5273a3db4c3dcec38673027de81fabfcdb99
Jonathan Luk <jluk87@gmail.com>
2019-05-15 02:27:12 +0000

    Automatically backed up by Learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,9 +1,6 @@
 class SessionsController < ApplicationController
 
   def new
-    if params[:name].blank? || params[:password].blank? || params[:password_confirmation]
-      render :new
-    elsif
 
   end
 
