b67e8cc2ed7322f9ee0c7fddec155307704179e5
Jonathan Luk <jluk87@gmail.com>
2019-05-15 03:02:09 +0000

    Automatically backed up by Learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,7 +5,13 @@ class SessionsController < ApplicationController
   end
 
   def create
-
+    user = User.find_by(name: params[:name])
+    if user.try(:authenicate, params[:password])
+      session[:user_id] = user.id
+      redirect_to 'welcome/index'
+    else
+      redirect_to new_session_path
+    end
   end
 
   def destroy
